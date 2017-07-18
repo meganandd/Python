6a66a9e6ac2cc0211ee0956198efee8c6985349d
Megan Andersen <mandersen.731@gmail.com>
2017-07-18 15:21:45 -0700

    Text game

--- textadv.py
+++ textadv.py
@@ -5,7 +5,7 @@ if user_input == "START":
 first_option = input("Do you 'go forward' or 'avoid the field'?")
 if first_option == "avoid the field":
     print("You decide to walk around the field instead. There is a dark and scary jungle that looks impossible to navigate. After entering the jungle, you fall into a deep pit and have no chance of escape. You lose.")
-    
+
 elif first_option == "go forward":
     print("After walking into the field, you see a threatening monster that towers over you.")
     first_choice = input("Will you 'run' or will you try to 'approach the monster'?")
@@ -13,3 +13,4 @@ elif first_option == "go forward":
         print("By running forward you startle the monster and it attacks you. You lose.")
     elif first_choice == "approach the monster":
         print("You walk slowly up to the beast. Despite its appearance, it is actually very kind and becomes your friend. You win!")
+ 
